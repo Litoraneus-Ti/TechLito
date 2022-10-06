95fa32aaaaac44a9018d403fb39cfaef15976bcc
Litoraneus-Ti <115177972+Litoraneus-Ti@users.noreply.github.com>
2022-10-06 13:52:52 -0300

    Add files via upload

---
+++ totvs.js
@@ -0,0 +1,18 @@
+let nome = document.querySelector('#nome')
+let labelNome = document.querySelector('#labelNome')
+let validNome = false;
+
+
+nome.addEventListener('keyup', () => {
+    if (nome.value.length <= 4) {
+        labelNome.setAttribute('style', 'color: red')
+        labelNome.innerHTML = 'Minimo de 5 caracteres'
+        nome.setAttribute('style', 'border-color: red')
+        validNome = false
+    } else {
+        labelNome.setAttribute('style', 'color: green')
+        labelNome.innerHTML = 'Nome Valido!'
+        nome.setAttribute('style', 'border-color: green')
+        validNome = true
+    }
+})
