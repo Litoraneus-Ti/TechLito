32d0e931252cfd9ea80be78e04de87c1af8b39e0
Litoraneus-Ti <115177972+Litoraneus-Ti@users.noreply.github.com>
2022-11-09 11:00:30 -0300

    Add files via upload

--- manutenção.js
+++ manutenção.js
@@ -20,21 +20,20 @@ nome.addEventListener('keyup', () => {
 })
 
 today = new Date();
-h = today.getHours();
-m = today.getMinutes();
-s = today.getSeconds();
+let h = String(today.getHours()).padStart(2, '0');
+let m = String(today.getMinutes()).padStart(2, '0');
+let s = String(today.getSeconds()).padStart(2, '0');
 const myDate = new Date().toLocaleDateString();
 console.log(myDate);
 const myInput = document.querySelector("#dataToday");
 myInput.value = myDate + "  " + h + ":" + m;
+console.log(myInput.value);
+
 
 
 let email = document.querySelector('#email')
 let labelEmail = document.querySelector('#labelEmail')
 let validEmail = false;
-
-
-
 email.addEventListener('keyup', () => {
 
     if (document.forms[0].email.value == ""
@@ -61,9 +60,35 @@ email.addEventListener('keyup', () => {
 
 //Substituição de email
 
-let Formulario = document.querySelector('#form-1');
+let Formulario = document.getElementById('form-1');
 
-let FormularioAtualizado = Formulario.replace("litoraneus", email.value);
+
+
+let FormularioAtualizado = Formulario.value = email.value;
+
+let textarea_2 = document.querySelector('#textarea_2');
+
+let Setor = document.getElementById('Setor');
+// let nivel_Urgencia = document.querySelector('#nivel_Urgencia');
+let textarea = document.querySelector('#textarea');
+//colocar _ abaixo no Grau_de_Urgência para funcionar lembrar de trocar!
+var ele = document.getElementsByName('Grau de Urgência');
+
+for (i = 0; i < ele.length; i++) {
+    if (ele[i].checked)
+        textarea_2.innerHTML = `Recebemos a sua solicitação, em breve retornaremos o pedido
+Nome: ${nome.value}
+Email: ${email.value}
+Setor: ${Setor.value}
+Nivel de Urgência: ${ele[i].value}
+Chamado solicitado: ${textarea_2.value}
+
+   `
+}
+
+
+
+
 
 
 
@@ -84,8 +109,7 @@ function receber() {
         )
 
         localStorage.setItem('listaUser', JSON.stringify(listaUser))
-        // let imgBack = document.getElementById('imagemreserva');
-        // imgBack.setAttribute('style', 'diplay:block');
+
         let Main = document.querySelector('.Main');
         Main.setAttribute('style', 'display:none')
         msgSuccess.setAttribute('style', 'display: block')
@@ -93,9 +117,7 @@ function receber() {
         msgError.setAttribute('style', 'display: none')
         msgError.innerHTML = ''
         // alert(`${nome.value} você receberá uma mensagem no email ${email.value}, caso não chegue repita o procedimento de cadastro, obrigado!`)
-        // setTimeout(() => {
-        //     window.location.href = ''
-        // }, 5000)
+
 
 
     } else {
@@ -104,6 +126,12 @@ function receber() {
         msgSuccess.innerHTML = ''
         msgSuccess.setAttribute('style', 'display: none')
     }
+
+
+    let form_2 = document.querySelector('#form_2').submit();
+    let AtivandoForm2 = form_2.submit()
+
+
 
 
 
